a3427f453326e138ad03ecf73430d3cdf5185433
parth-b <iamparthbansal@gmail.com>
2020-10-10 01:57:16 +0530

    Test

--- lib/ui/Login.dart
+++ lib/ui/Login.dart
@@ -4,7 +4,9 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text("Login"),
+      ),
     );
   }
 }
