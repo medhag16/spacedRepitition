137be32307c93f5f8124e8c90e7816ab13be23ae
Medha Gupta <guptamedha789@gmail.com>
2020-10-12 18:29:26 +0530

    added bottom nav,popup

--- lib/ui/myHome.dart
+++ lib/ui/myHome.dart
@@ -2,27 +2,64 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:spaced_rep_app/ui/Register.dart';
+import 'package:motion_tab_bar/MotionTabBarView.dart';
+import 'package:motion_tab_bar/MotionTabController.dart';
+import 'package:motion_tab_bar/TabItem.dart';
+import 'package:motion_tab_bar/motiontabbar.dart';
 
-/*void choiceAction(String choice) {
-  signOutGoogle();
-  /*Navigator.of(context).pushAndRemoveUntil(
-      MaterialPageRoute(builder: (context) {
-    return Register();
-  }), ModalRoute.withName('/'));*/
-}*/
+MotionTabController _tabController;
 
 class Constants {
   static const String SignOut = 'Sign out';
-
   static const List<String> choices = <String>[SignOut];
 }
 
-class MyApp extends StatelessWidget {
+String cardname = "";
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+  //AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    //_controller = AnimationController(vsync: this);
+    _tabController = new MotionTabController(initialIndex: 1, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    //_controller.dispose();
+    _tabController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        theme: new ThemeData(
+          primarySwatch: Colors.pink,
+        ),
         debugShowCheckedModeBanner: false,
         home: Scaffold(
+          bottomNavigationBar: MotionTabBar(
+            labels: ["Account", "Home", "Dashboard"],
+            initialSelectedTab: "Home",
+            tabIconColor: Colors.green,
+            tabSelectedColor: Colors.red,
+            onTabItemSelected: (int value) {
+              print(value);
+              setState(() {
+                _tabController.index = value;
+              });
+            },
+            icons: [Icons.account_box, Icons.home, Icons.menu],
+            textStyle: TextStyle(color: Colors.red),
+          ),
           drawer: Drawer(
               child: ListView(
             children: <Widget>[
@@ -123,17 +160,17 @@ class MyApp extends StatelessWidget {
                   child: Icon(Icons.book),
                   //backgroundColor: Colors.amber,
                   label: 'Add Subject',
-                  onTap: () => print('Subject Added')),
+                  onTap: _showMyDialog),
               SpeedDialChild(
                   child: Icon(Icons.file_upload),
                   //backgroundColor: Colors.amber,
                   label: 'Upload Card',
-                  onTap: () => print('Card Added')),
+                  onTap: () => print('Card Uploaded')),
               SpeedDialChild(
                   child: Icon(Icons.share),
                   backgroundColor: Colors.blue,
                   label: 'Share Card',
-                  onTap: () => print('Added')),
+                  onTap: () => print('Share')),
             ],
           ),
           appBar: AppBar(
@@ -145,20 +182,13 @@ class MyApp extends StatelessWidget {
             actions: <Widget>[
               Icon(Icons.notifications),
               FlatButton(
-                onPressed: () {
-                  signOutGoogle();
-                  Navigator.of(context).pushAndRemoveUntil(
-                      MaterialPageRoute(builder: (context) {
-                    return Register();
-                  }), ModalRoute.withName('/'));
-                },
+                onPressed: () {},
                 child: CircleAvatar(
                   radius: 20,
                   backgroundImage: NetworkImage(user.photoUrl),
                 ),
               ),
               PopupMenuButton<String>(
-                //onSelected: choiceAction,
                 itemBuilder: (BuildContext context) {
                   return Constants.choices.map((String choice) {
                     return PopupMenuItem<String>(
@@ -180,6 +210,43 @@ class MyApp extends StatelessWidget {
           ),
           body: MyHome(),
         ));
+  }
+
+  Future<void> _showMyDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Colors.blueGrey[100],
+          elevation: 20,
+          title: Text('Add Card'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(labelText: "Enter Card Name..."),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Save'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            FlatButton(
+              child: Text('Discard'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 }
 
