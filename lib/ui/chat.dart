ef27af7e5fc785aaa355841082659c7124b4e578
Medha Gupta <guptamedha789@gmail.com>
2020-10-13 16:04:44 +0530

    idk

---
+++ lib/ui/chat.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyChat extends StatefulWidget {
+  @override
+  _MyChatState createState() => _MyChatState();
+}
+
+class _MyChatState extends State<MyChat> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("dhb"),
+    );
+  }
+}
